c9288b0bb0b5263b28aa213609a520f1363c66c6
Richard Neumann <mail@richard-neumann.de>
2021-10-01 12:16:02 +0200

    Add further session management functions

--- cshsso/session.py
+++ cshsso/session.py
@@ -1,12 +1,21 @@
 """Manage sessions."""
 
-from flask import request
+from datetime import datetime, timedelta
 
+from flask import request, Response
+
+from cshsso.config import CONFIG
 from cshsso.exceptions import NotLoggedIn
+from cshsso.functions import genpw
+from cshsso.localproxies import SESSION
 from cshsso.orm import Account, Session
 
 
-__all__ = ['get_session']
+__all__ = ['get_session', 'create', 'renew', 'set_cookies']
+
+
+DEFAULT_DURATION = 60 * 60      # One hour.
+MAX_DURATION = 24 * 60 * 60     # One day.
 
 
 def get_session() -> Session:
@@ -28,3 +37,48 @@ def get_session() -> Session:
         return session
 
     raise NotLoggedIn()
+
+
+def get_duration() -> timedelta:
+    """Returns the session duration."""
+
+    try:
+        duration = int(request.cookies['cshsso-session-duration'])
+    except (KeyError, ValueError):
+        duration = CONFIG.getint('session', 'duration',
+                                 fallback=DEFAULT_DURATION)
+
+    return timedelta(seconds=min((duration, MAX_DURATION)))
+
+
+def get_deadline() -> datetime:
+    """Returns the session deadline."""
+
+    return datetime.now() + get_duration()
+
+
+def create(account: Account) -> tuple[int, str]:
+    """Creates a new session for the given account."""
+
+    password = genpw()
+    session = Session(account=account, deadline=get_deadline(),
+                      password=password)
+    session.save()
+    return (session.id, password)
+
+
+def renew(session: Session) -> tuple[int, str]:
+    """Renews the session and returns the new password."""
+
+    session.password = password = genpw()
+    session.deadline = get_deadline()
+    session.save()
+    return (session.id, password)
+
+
+def set_cookies(response: Response) -> None:
+    """Sets session cookies."""
+
+    ident, password = renew(SESSION)
+    response.set_cookie('cshsso-session-id', str(ident))
+    response.set_cookie('cshsso-session-password', password)
