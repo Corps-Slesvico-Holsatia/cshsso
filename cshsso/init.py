0bd97cdca799521853197a15cc608f8067ee0c50
Richard Neumann <mail@richard-neumann.de>
2021-10-05 16:21:44 +0200

    Add init.py

---
+++ cshsso/init.py
@@ -0,0 +1,18 @@
+"""Configuration initialization."""
+
+from cshsso.config import CONFIG, CONFIG_FILE
+from cshsso.orm import DATABASE
+
+
+__all__ = ['init']
+
+
+def init() -> None:
+    """Initializes the app."""
+
+    CONFIG.read(CONFIG_FILE)
+    DATABASE.init(DATABASE.database,
+                  host=CONFIG.get('db', 'host'),
+                  user=CONFIG.get('db', 'user'),
+                  passwd=CONFIG.get('db', 'passwd')
+    )
