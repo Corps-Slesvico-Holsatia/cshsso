20add1a0c30dfc4fda5136e787941feb66c28f12
Richard Neumann <mail@richard-neumann.de>
2021-10-05 22:03:35 +0200

    Rewrite authorization checks

--- cshsso/authorization.py
+++ cshsso/authorization.py
@@ -1,25 +1,46 @@
 """Authorization checks."""
 
 from cshsso.orm import User
-from cshsso.roles import Group
+from cshsso.roles import Circle, CommissionGroup
 
 
-__all__ = ['check_group_authorization']
+__all__ = ['check_minimum_circle', 'check_commission_group']
 
 
-def check_group_authorization(user: User, group: Group) -> bool:
+def is_in_inner_circle(user: User) -> bool:
+    """Checks whether the user is member of the inner circle."""
+
+    return user.status in Circle.INNER
+
+
+def is_in_outer_circle(user: User) -> bool:
+    """Checks whether the user is member of the outer circle."""
+
+    return user.status in Circle.OUTER
+
+
+def is_guest(user: User) -> bool:
+    """Checks whether the user is a guest."""
+
+    return user.status in Circle.GUESTS
+
+
+def check_minimum_circle(user: User, circle: Circle) -> bool:
     """Determines whether the user is authorized for the given group."""
 
-    if group == Group.CHARGES:
-        return any(charge in Group.CHARGES for charge in user.charges)
+    if circle is Circle.INNER:
+        return is_in_inner_circle(user)
 
-    if user.role in Group.INNER:
+    if circle is Circle.OUTER:
+        return is_in_inner_circle(user) or is_in_outer_circle(user)
+
+    if circle is Circle.GUESTS:
         return True
 
-    if user.role in Group.OUTER:
-        return group in {Group.OUTER, Group.GUEST}
-
-    if user.role in Group.GUEST:
-        return group == Group.GUEST
-
     return False
+
+
+def check_commission_group(user: User, group: CommissionGroup) -> bool:
+    """Checks whether the user is charged."""
+
+    return any(uc.commission in group for uc in user.commissions)
