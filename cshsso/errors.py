051213d04277cbf94e991ba0728b69f0723616b9
Richard Neumann <mail@richard-neumann.de>
2021-10-06 00:12:14 +0200

    Add handling of VerificationError

--- cshsso/errors.py
+++ cshsso/errors.py
@@ -1,5 +1,8 @@
 """Common error handlers."""
 
+from flask import jsonify
+from recaptcha import VerificationError
+
 from cshsso.orm import User, Session
 
 
@@ -8,5 +11,6 @@ __all__ = ['ERRORS']
 
 ERRORS = {
     User.DoesNotExist: lambda _: ('No such user.', 404),
-    Session.DoesNotExist: lambda _: ('No such session.', 404)
+    Session.DoesNotExist: lambda _: ('No such session.', 404),
+    VerificationError: lambda error: (jsonify(error.json), 400)
 }
