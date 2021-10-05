a49be67ba786f9514b61cd0794a657281d6908de
Richard Neumann <mail@richard-neumann.de>
2021-10-05 16:26:11 +0200

    Export library interface

--- cshsso/__init__.py
+++ cshsso/__init__.py
@@ -1,1 +1,18 @@
 """Single Sign-On framework for the Corps."""
+
+from cshsso.application import Application
+from cshsso.init import init
+from cshsso.orm import User
+from cshsso.decorators import authenticated, authorized
+from cshsso.localproxies import SESSION, USER
+
+
+__all__ = [
+    'SESSION',
+    'USER',
+    'Application',
+    'User',
+    'authenticated',
+    'authorized',
+    'init'
+]
