cdad2e1531e571973c4f275b0c7de71cb8f4302e
Richard Neumann <mail@richard-neumann.de>
2021-10-05 17:33:07 +0200

    Add NewSession

--- cshsso/typing.py
+++ cshsso/typing.py
@@ -1,12 +1,21 @@
 """Common type hints."""
 
-from typing import Callable
+from typing import Callable, NamedTuple
 
 from flask import Response
 
+from cshsso.orm import Session
 
-__all__ = ['ErrorHandler', 'ErrorHandlers']
+
+__all__ = ['ErrorHandler', 'ErrorHandlers', 'NewSession']
 
 
 ErrorHandler = Callable[[Exception], Response]
 ErrorHandlers = dict[type, ErrorHandler]
+
+
+class NewSession(NamedTuple):
+    """Representas a new session with its password in clear text."""
+
+    session: Session
+    passwd: str
