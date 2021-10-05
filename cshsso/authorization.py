fd8baa5a0f5de80731849fc0a0cc3f78f57e6e5e
Richard Neumann <mail@richard-neumann.de>
2021-10-06 00:51:47 +0200

    Add check_target()

--- cshsso/authorization.py
+++ cshsso/authorization.py
@@ -1,10 +1,12 @@
 """Authorization checks."""
 
+from typing import Union
+
 from cshsso.orm import User
 from cshsso.roles import Circle, CommissionGroup
 
 
-__all__ = ['check_minimum_circle', 'check_commission_group']
+__all__ = ['check_minimum_circle', 'check_commission_group', 'check_target']
 
 
 def is_in_inner_circle(user: User) -> bool:
@@ -44,3 +46,15 @@ def check_commission_group(user: User, group: CommissionGroup) -> bool:
     """Checks whether the user is charged."""
 
     return any(uc.commission in group for uc in user.commissions)
+
+
+def check_target(user: User, target: Union[Circle, CommissionGroup]) -> bool:
+    """Checks a user against a Corps circle or commission group."""
+
+    if isinstance(target, Circle):
+        return check_minimum_circle(user, target)
+
+    if isinstance(target, CommissionGroup):
+        return check_commission_group(user, target)
+
+    raise TypeError('Must specify either Circle or CommissionGroup.')
