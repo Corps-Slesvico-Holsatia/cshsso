f4a5ce23179e04a06d5e2e06432d30de11d57929
Richard Neumann <mail@richard-neumann.de>
2021-10-05 21:16:24 +0200

    Fix group auth check

--- cshsso/authorization.py
+++ cshsso/authorization.py
@@ -10,13 +10,16 @@ __all__ = ['check_group_authorization']
 def check_group_authorization(user: User, group: Group) -> bool:
     """Determines whether the user is authorized for the given group."""
 
-    if user.group == Group.INNER:
+    if group == Group.CHARGES:
+        return any(charge in Group.CHARGES for charge in user.charges)
+
+    if user.role in Group.INNER:
         return True
 
-    if user.group == Group.OUTER:
+    if user.role in Group.OUTER:
         return group in {Group.OUTER, Group.GUEST}
 
-    if user.group == Group.GUEST:
+    if user.role in Group.GUEST:
         return group == Group.GUEST
 
     return False
