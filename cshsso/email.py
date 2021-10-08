5a78ca69120f68261424b85bcd63687b3390262e
Richard Neumann <mail@richard-neumann.de>
2021-10-08 11:38:28 +0200

    Add emailing mnodule

---
+++ cshsso/email.py
@@ -0,0 +1,27 @@
+"""Sending emails."""
+
+from functools import cache
+from typing import Iterable
+
+from emaillib import EMail, Mailer
+
+from cshsso.config import CONFIG
+
+
+__all__ = ['send']
+
+
+@cache
+def get_mailer(*, section: str = 'email') -> Mailer:
+    """Returns the mailer as per the configuration."""
+
+    return Mailer(
+        CONFIG.get(section, 'server'), CONFIG.getint(section, 'port'),
+        CONFIG.get(section, 'login'), CONFIG.get(section, 'passwd')
+    )
+
+
+def send(emails: Iterable[EMail]) -> bool:
+    """Sends the provided emails."""
+
+    return get_mailer().send(emails)
