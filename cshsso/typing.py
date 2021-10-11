7616e902c4dbea685759cf87e30276139a4f61a5
Richard Neumann <mail@richard-neumann.de>
2021-10-11 17:16:11 +0200

    Correct Decorator type hint

--- cshsso/typing.py
+++ cshsso/typing.py
@@ -8,6 +8,6 @@ from flask import Response
 __all__ = ['Decorator', 'ErrorHandler', 'ErrorHandlers']
 
 
-Decorator = Callable[Callable[..., Any], Callable[..., Any]]
+Decorator = Callable[[Callable[..., Any]], Callable[..., Any]]
 ErrorHandler = Callable[[Exception], Response]
 ErrorHandlers = dict[type, ErrorHandler]
