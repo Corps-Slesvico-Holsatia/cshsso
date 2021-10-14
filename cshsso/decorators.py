f191de276439e47d493db9263c5da94c7a9fbb33
Richard Neumann <mail@richard-neumann.de>
2021-10-15 00:31:37 +0200

    Add decorators for convent-related authorization

--- cshsso/decorators.py
+++ cshsso/decorators.py
@@ -2,9 +2,10 @@
 
 from enum import Enum
 from functools import wraps
-from typing import Any, Callable, Union
+from typing import Any, Callable
 
-from cshsso.authorization import check_target
+from cshsso.authorization import AuthorizationTarget, check_target
+from cshsso.convents import Convent, ConventAuthorization
 from cshsso.exceptions import NotAuthenticated, NotAuthorized
 from cshsso.localproxies import USER, SESSION
 from cshsso.roles import Circle, CommissionGroup
@@ -27,7 +28,7 @@ def authenticated(function: Callable[..., Any]) -> Callable[..., Any]:
     return wrapper
 
 
-def authorized(target: Union[Circle, CommissionGroup]) -> Decorator:
+def authorized(target: AuthorizationTarget) -> Decorator:
     """Determines whether the current user is authorized."""
 
     def decorator(function: Callable[..., Any]) -> Callable[..., Any]:
@@ -59,11 +60,22 @@ def admin(function: Callable[..., Any]) -> Callable[..., Any]:
 class Authorization(Enum):
     """Authorization modes."""
 
+    # Circles
     INNER = authorized(Circle.INNER)
     OUTER = authorized(Circle.OUTER)
     GUESTS = authorized(Circle.GUESTS)
+    # Commissions
     CHARGES = authorized(CommissionGroup.CHARGES)
     AHV = authorized(CommissionGroup.AHV)
+    # Convents
+    AHC = authorized(ConventAuthorization(Convent.AHC))
+    AHC_VOTE = authorized(ConventAuthorization(Convent.AHC, True))
+    CC = authorized(ConventAuthorization(Convent.CC))
+    CC_VOTE = authorized(ConventAuthorization(Convent.CC, True))
+    FC = authorized(ConventAuthorization(Convent.FC))
+    FC_VOTE = authorized(ConventAuthorization(Convent.FC, True))
+    FCC = authorized(ConventAuthorization(Convent.FCC))
+    FCC_VOTE = authorized(ConventAuthorization(Convent.FCC, True))
 
     def __call__(self, *args, **kwargs) -> Any:
         """Delegate to decorator function."""
