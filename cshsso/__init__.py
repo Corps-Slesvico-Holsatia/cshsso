c246d13b0f85e09cc30fb01646237cde018591cf
Richard Neumann <mail@richard-neumann.de>
2021-10-14 23:00:54 +0200

    Export Semester

--- cshsso/__init__.py
+++ cshsso/__init__.py
@@ -5,6 +5,7 @@ from cshsso.decorators import authenticated, authorized, Authorization
 from cshsso.init import init
 from cshsso.localproxies import SESSION, USER
 from cshsso.orm import User
+from cshsso.semester import Semester
 from cshsso.wsgi import APPLICATION
 
 
@@ -14,6 +15,7 @@ __all__ = [
     'USER',
     'Application',
     'Authorization',
+    'Semester',
     'User',
     'authenticated',
     'authorized',
