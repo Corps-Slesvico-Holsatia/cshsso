56283bb8668e13795287167c7d4923988d27559c
Richard Neumann <mail@richard-neumann.de>
2021-10-14 23:11:55 +0200

    Add to_json() methods

--- cshsso/roles.py
+++ cshsso/roles.py
@@ -54,6 +54,10 @@ class Circle(Enum):
     def __contains__(self, value: Any) -> bool:
         return value in self.value
 
+    def to_json(self) -> list[dict]:
+        """Returns a JSON-ish list."""
+        return [status.to_json() for status in self.value]
+
 
 class Commission(Enum):
     """Commission types."""
@@ -91,3 +95,7 @@ class CommissionGroup(Enum):
 
     def __contains__(self, value: Any) -> bool:
         return value in self.value
+
+    def to_json(self) -> list[dict]:
+        """Returns a JSON-ish list."""
+        return [status.to_json() for status in self.value]
