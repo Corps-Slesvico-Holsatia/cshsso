0ec289385fa19e355c047f3084d9ebeca3d0d07c
Richard Neumann <mail@richard-neumann.de>
2021-10-14 22:53:06 +0200

    Add account handling

--- cshsso/wsgi/__init__.py
+++ cshsso/wsgi/__init__.py
@@ -4,6 +4,11 @@ from cshsso.application import Application
 from cshsso.wsgi.login import login
 from cshsso.wsgi.logout import logout
 from cshsso.wsgi.register import register
+from cshsso.wsgi.account import show as show_account
+from cshsso.wsgi.account import patch as patch_account
+from cshsso.wsgi.account import delete as delete_account
+from cshsso.wsgi.account import set_status
+from cshsso.wsgi.account import set_commissions
 
 
 __all__ = ['APPLICATION']
@@ -13,3 +18,9 @@ APPLICATION = Application('CSHSSO')
 APPLICATION.route('/login', methods=['POST'])(login)
 APPLICATION.route('/logout', methods=['POST'])(logout)
 APPLICATION.route('/register', methods=['POST'])(register)
+APPLICATION.route('/account', methods=['GET'])(show_account)
+APPLICATION.route('/account', methods=['PATCH'])(patch_account)
+APPLICATION.route('/account/delete', methods=['POST'])(delete_account)
+APPLICATION.route('/account/status', methods=['POST'])(set_status)
+APPLICATION.route('/account/commissions', methods=['POST'])(
+    set_commissions)
---
+++ cshsso/wsgi/account.py
@@ -0,0 +1,125 @@
+"""Manage accounts."""
+
+from typing import Optional
+
+from flask import request
+
+from wsgilib import JSON, JSONMessage
+
+from cshsso.decorators import authenticated, Authorization
+from cshsso.exceptions import InvalidPassword
+from cshsso.localproxies import SESSION, USER
+from cshsso.orm import Session, User, UserCommission
+from cshsso.roles import Commission, Status
+
+
+__all__ = ['show', 'patch', 'delete', 'set_status', 'set_commissions']
+
+
+USER_VIEW_FIELDS = frozenset({
+    'email', 'first_name', 'last_name', 'status', 'registered'
+})
+USER_PATCH_FIELDS = frozenset({'first_name', 'last_name'})
+
+
+def get_user_as_json(session: Session, user: User) -> dict:
+    """Returns the current user as JSON."""
+
+    if session.user.admin:
+        return user.to_json()
+
+    return user.to_json(only=USER_VIEW_FIELDS)
+
+
+def patch_user(session: Session, user: User, json: dict) -> User:
+    """Patches the user."""
+
+    if session.user.admin:
+        return user.patch_json(json).save()
+
+    return user.patch_json(json, only=USER_PATCH_FIELDS).save()
+
+
+def delete_user(session: Session, user: User, *,
+                passwd: Optional[str] = None) -> bool:
+    """Deletes the user."""
+
+    if session.user.admin:
+        return user.delete_instance()
+
+    if passwd is not None and user.login(passwd):
+        return user.delete_instance()
+
+    raise InvalidPassword()
+
+
+@authenticated
+def show() -> JSON:
+    """Shows the user's profile."""
+
+    return JSON(get_user_as_json(SESSION, USER))
+
+
+@authenticated
+def patch() -> JSONMessage:
+    """Updates the user's profile."""
+
+    user = patch_user(SESSION, USER, request.json)
+    user.save()
+    return JSONMessage('User patched.', status=200)
+
+
+@authenticated
+def delete() -> JSONMessage:
+    """Deletes the user's account."""
+
+    delete_user(SESSION, USER, passwd=request.json.get('passwd'))
+    return JSONMessage('User deleted.', status=200)
+
+
+@authenticated
+@Authorization.CHARGES
+def set_status() -> JSONMessage:
+    """Sets the status of a user."""
+
+    try:
+        status = Status[request.json['status']]
+    except KeyError:
+        return JSONMessage('No status provied.', status=400)
+    except ValueError:
+        return JSONMessage('Invalid status provied.', status=400)
+
+    user = USER._get_current_object()   # pylint: disable=W0212
+    old_status, user.status = user.status, status
+    user.save()
+    return JSONMessage('Status updated.', old=old_status.name, new=status.name,
+                       status=200)
+
+
+@authenticated
+@Authorization.CHARGES
+def set_commissions() -> JSONMessage:
+    """Sets the commissions for a user."""
+
+    try:
+        commissions = {Commission[c] for c in request.json['commissions']}
+    except KeyError:
+        return JSONMessage('No commissions provied.', status=400)
+    except ValueError:
+        return JSONMessage('Invalid commission provied.', status=400)
+
+    user = USER._get_current_object()   # pylint: disable=W0212
+    old_commissions = set()
+
+    for user_commission in user.commissions:
+        old_commissions.add(user_commission.commission)
+        user_commission.delete_instance()
+
+    for commission in commissions:
+        user_commission = UserCommission(user=user, commission=commission)
+        user_commission.save()
+
+    return JSONMessage('Commissions updated.',
+                       old={c.name for c in old_commissions},
+                       new={c.name for c in commissions},
+                       status=200)
