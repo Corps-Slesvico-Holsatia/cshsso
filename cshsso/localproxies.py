a17bc9cdc63c77586ad067556622e08e2a5a0636
Richard Neumann <mail@richard-neumann.de>
2021-12-04 00:15:46 +0100

    Use get_user() from ormfuncs.py

--- cshsso/localproxies.py
+++ cshsso/localproxies.py
@@ -3,31 +3,29 @@
 from functools import partial
 
 from flask import request
-from peewee import JOIN
 from werkzeug.local import LocalProxy
 
-from cshsso.orm import Session, User, UserCommission
+from cshsso.orm import Session, User
+from cshsso.ormfuncs import get_user
 from cshsso.session import get_session
 
 
 __all__ = ['SESSION', 'USER']
 
 
-def get_user(session: Session) -> User:
+def get_current_user(session: Session) -> User:
     """Returns the current user."""
 
     if not session.user.admin:
         return session.user
 
     try:
-        uid = request.cookies['cshsso-user-id']
+        uid = int(request.cookies['cshsso-user-id'])
     except KeyError:
         return session.user
 
-    return User.select(User, UserCommission).join(
-        UserCommission, on=UserCommission.user == User.id,
-        join_type=JOIN.LEFT_OUTER).where(User.id == uid).group_by(User).get()
+    return get_user(uid)
 
 
 SESSION = LocalProxy(get_session)
-USER = LocalProxy(partial(get_user, SESSION))
+USER = LocalProxy(partial(get_current_user, SESSION))
