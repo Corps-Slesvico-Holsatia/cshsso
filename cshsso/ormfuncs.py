f0ca82d0487bb7b2642dc6c2140a746f2f6d7071
Richard Neumann <mail@richard-neumann.de>
2021-12-04 00:17:04 +0100

    Add pass_commission()

--- cshsso/ormfuncs.py
+++ cshsso/ormfuncs.py
@@ -3,9 +3,10 @@
 from peewee import JOIN
 
 from cshsso.orm import User, UserCommission
+from cshsso.roles import Commission
 
 
-__all__ = ['get_user']
+__all__ = ['get_user', 'pass_commission']
 
 
 def get_user(uid: int) -> User:
@@ -14,3 +15,19 @@ def get_user(uid: int) -> User:
     return User.select(User, UserCommission).join(
         UserCommission, on=UserCommission.user == User.id,
         join_type=JOIN.LEFT_OUTER).where(User.id == uid).group_by(User).get()
+
+
+def pass_commission(commission: Commission, src: User, dst: User) -> None:
+    """Passes a commission from one user to another."""
+
+    try:
+        src_commission = UserCommission.get(UserCommission.occupant == src)
+    except UserCommission.DoesNotExist:
+        src_commission = None
+
+    if src.admin or src_commission is not None:
+        dst_commission = UserCommission(occupant=dst, commission=commission)
+        dst_commission.save()
+
+    if src_commission is not None:
+        src_commission.delete_instance()
