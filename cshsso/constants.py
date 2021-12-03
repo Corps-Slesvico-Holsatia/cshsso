b0fa9237a99106001933981b2adb071678d9c5ae
Richard Neumann <mail@richard-neumann.de>
2021-12-03 23:39:53 +0100

    Introduce constants.py
    Use SESSION_ID and SESSION_SECRET to access the respective cookies.

---
+++ cshsso/constants.py
@@ -0,0 +1,7 @@
+"""Common hard-coded constants."""
+
+__all__ = ['SESSION_ID', 'SESSION_SECRET']
+
+
+SESSION_ID = 'cshsso-session-id'
+SESSION_SECRET = 'cshsso-session-secret'
--- cshsso/session.py
+++ cshsso/session.py
@@ -6,6 +6,7 @@ from peewee import JOIN
 from werkzeug.http import dump_cookie
 
 from cshsso.config import CONFIG
+from cshsso.constants import SESSION_ID, SESSION_SECRET
 from cshsso.exceptions import NotLoggedIn
 from cshsso.functions import genpw
 from cshsso.orm import User, Session, UserCommission
@@ -24,8 +25,8 @@ def get_session() -> Session:
     """Returns the current session object."""
 
     try:
-        session_id = request.cookies['cshsso-session-id']
-        secret = request.cookies['cshsso-session-secret']
+        session_id = request.cookies[SESSION_ID]
+        secret = request.cookies[SESSION_SECRET]
     except KeyError:
         raise NotLoggedIn() from None
 
@@ -45,35 +46,11 @@ def get_session() -> Session:
     return session
 
 
-def create(user: User) -> tuple[Session, str]:
+def for_user(user: User) -> tuple[Session, str]:
     """Opens a new session for the given user."""
 
     session = Session(user=user, secret=(secret := genpw()))
     return (session, secret)
-
-
-def for_user(user: User) -> tuple[Session, str]:
-    """Returns a session and a session password for the given user."""
-
-    sessions = set()
-
-    for session in Session.select().where(Session.user == user):
-        if session.active:
-            sessions.add(session)
-        else:
-            session.delete_instance()
-
-    try:
-        *old, last = sessions
-    except ValueError:
-        return create(user=user)
-
-    for session in old:
-        session.delete_instance()
-
-    last.secret = secret = genpw()
-    last.save()
-    return (last, secret)
 
 
 def set_cookie(response: Response, *args, **kwargs):
@@ -100,10 +77,10 @@ def set_session_cookies(response: Response, session: Session,
 
     for domain in CONFIG.get('auth', 'domains').split():
         set_cookie(
-            response, 'cshsso-session-id', str(session.id),
+            response, SESSION_ID, str(session.id),
             expires=session.end, domain=domain, secure=True, samesite=None)
         set_cookie(
-            response, 'cshsso-session-secret', secret,
+            response, SESSION_SECRET, secret,
             expires=session.end, domain=domain, secure=True, samesite=None)
 
     return response
