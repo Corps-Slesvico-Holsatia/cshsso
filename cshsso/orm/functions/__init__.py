083c6cee2b4e1799b812e2157ee622bb73163e8f
Richard Neumann <mail@richard-neumann.de>
2021-12-04 14:42:18 +0100

    Order exports

--- cshsso/orm/functions/__init__.py
+++ cshsso/orm/functions/__init__.py
@@ -2,16 +2,16 @@
 
 from cshsso.orm.functions.commissions import set_commissions
 from cshsso.orm.functions.user import delete_user
-from cshsso.orm.functions.user import get_user
 from cshsso.orm.functions.user import get_current_user
-from cshsso.orm.functions.user import user_to_json
+from cshsso.orm.functions.user import get_user
 from cshsso.orm.functions.user import patch_user
+from cshsso.orm.functions.user import user_to_json
 
 __all__ = [
     'delete_user',
-    'get_user',
     'get_current_user',
-    'user_to_json',
+    'get_user',
     'patch_user',
-    'set_commissions'
+    'set_commissions',
+    'user_to_json'
 ]
